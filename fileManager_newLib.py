a99380ba69b40f5d7f10a50ec4c9923b32d72491
William Coutinho <william.csouza@hotmail.com>
2020-01-26 15:32:38 -0300

    folderManager e filesManager com nova biblioteca pathlib + utilização de função para esconder/mostrar traceback quando solicitado

---
+++ fileManager_newLib.py
@@ -0,0 +1,74 @@
+#!-*- conding: utf8 -*-
+from pathlib import Path as path_manager
+import sys
+import logging
+
+debug = False # Used to avoid Traceback errors when Fails
+
+PATH_FOLDER = path_manager("Version_control")
+FILE_NAME = "version_control.txt"
+PATH_COMPLETE = PATH_FOLDER / FILE_NAME 
+#OLDER_BUILD = "6.1.2.55"
+LATEST_BUILD = "6.1.1.3" 
+
+
+def pathControl(LATEST_BUILD):
+    if PATH_FOLDER.exists() and PATH_COMPLETE.exists():
+        try:
+            print('Checking latest build downloaded')
+            return checkLatestBuildDownloaded(LATEST_BUILD)                  
+                
+        except (EnvironmentError, IOError, OSError) as e:
+            return e
+            
+    elif not(PATH_FOLDER.exists()):
+        print('Folder not found:', PATH_FOLDER)
+        return 0
+    
+    else:
+        print('File not exists. Creating a new one...')
+        return createFileControl(LATEST_BUILD)
+        
+
+def checkLatestBuildDownloaded(LATEST_BUILD):
+    with PATH_COMPLETE.open() as version_control:
+        try:
+            OLDER_BUILD = version_control.read()
+            if OLDER_BUILD == '':
+                print('File is empty. Creating a new one with build name received:',LATEST_BUILD)
+                createFileControl(LATEST_BUILD)
+                return False
+            elif OLDER_BUILD != LATEST_BUILD and OLDER_BUILD != '':
+                print(f'Build {LATEST_BUILD} received is different of {OLDER_BUILD} from history.')
+                print('Updating latest build from local file...')
+                createFileControl(LATEST_BUILD)
+                return False
+            else:
+                print(f'Build {LATEST_BUILD} received is the same of {OLDER_BUILD} from history.')
+                print('File not updated.')
+                return True
+        except (EnvironmentError, IOError, OSError)  as e:
+            print('Not possible to open history file:', e)
+            return e
+
+def createFileControl(LATEST_BUILD):
+    with PATH_COMPLETE.open('w') as version_control:
+        try:
+            version_control.write(LATEST_BUILD)
+            print('File updated to:', LATEST_BUILD)
+            return LATEST_BUILD
+        except (EnvironmentError, IOError, OSError)  as e:
+            print('Not possible to open history file:', e)
+            return e    
+
+# Function to not print traceback unless debug is True
+def exceptionHandler(exception_type, exception, traceback, debug_hook=sys.excepthook):
+    if debug:
+        print('\n*** Error ***')
+        # Display all exceptions catched using debug_hook
+        debug_hook(exception_type, exception, traceback)
+    else:
+        print(f'\t{exception_type.__name__}: {exception}')# Exception.name: exception
+sys.excepthook = exceptionHandler    
+    
+pathControl(LATEST_BUILD)
---
+++ folderManager_newLib.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+from pathlib import Path as path_manager
+import sys
+import logging
+
+debug = False # Used to avoid Traceback errors when Fails
+
+
+#caminho até as Builds: \\10.4.164.16\\sao\\01.Opentv5\\00.Versions\\NET\\6.1Release
+path_sub_folder_release = ['release_netbrazil_4tnfx_uhd_hdcp_nasc_integration_noOTA','release_netbrazil_4tnfx_uhd_hdcp_nasc_integration_OTA','release_netbrazil_4tnfx_uhd_hdcp_nasc_production_OTA']
+path_sub_folder_dev = ['dev_netbrazil_4tnfx_uhd_hdcp_nasc_integration_noOTA','dev_netbrazil_4tnfx_uhd_hdcp_nasc_integration_OTA','dev_netbrazil_4tnfx_uhd_hdcp_nasc_production_OTA']
+folders_name = ['build_product_release','build_product_sdk','build_product_DEV'] #variavel que virá do sftp
+sub_folder_name = ['release_netbrazil_4tnfx_uhd_hdcp_nasc_integration_noOTA','release_netbrazil_4tnfx_uhd_hdcp_nasc_integration_OTA','release_netbrazil_4tnfx_uhd_hdcp_nasc_production_OTA']#variavel que virá do sftp
+build_name = '6.1.2.10'
+LOCAL_PATH = path_manager('C:/Users/William/Desktop/SFTP Project')
+
+def createBuildNameFolder(build_name):
+    if(LOCAL_PATH.exists()):
+        try:
+            print(f'Creating {build_name} folder')
+            path_manager.mkdir(LOCAL_PATH / build_name)
+            return LOCAL_PATH / build_name
+        
+        except (EnvironmentError, IOError, OSError) as e:
+            print(e)
+    
+    elif not(LOCAL_PATH.exists()):
+        print('Path not found.')
+        
+def createLocalFolders(new_path,folder_name):
+    try:
+        path_manager.mkdir(new_path / folder_name)
+        return new_path / folder_name
+    
+    except (EnvironmentError, IOError, OSError) as e:
+        print(e)
+    
+def createLocalSubFolders(new_path, sub_folder_name):
+    try:
+        path_manager.mkdir(new_path / sub_folder_name)
+        return new_path / sub_folder_name
+    
+    except (EnvironmentError, IOError, OSError) as e:
+        print(e)
+  
+# Function to not print traceback unless debug is True
+def exceptionHandler(exception_type, exception, traceback, debug_hook=sys.excepthook):
+    if debug:
+        print('\n*** Error ***')
+        # Display all exceptions catched using debug_hook
+        debug_hook(exception_type, exception, traceback)
+    else:
+        print(f'\t{exception_type.__name__}: {exception}')# Exception.name: exception
+sys.excepthook = exceptionHandler
+
+### Test ### 
+
+new_local_path = LOCAL_PATH / createBuildNameFolder(build_name)
+path_sub_folder = path_sub_folder_release
+path_product_folder = folders_name
+for sdk in path_product_folder:
+    if 'build_product_sdk' == sdk:
+        path_product_folder.remove(sdk)
+        
+for folder_name in path_product_folder:
+    try:
+        #wa_new_path = new_local_path #WorkAround!
+        new_local_path = new_local_path / createLocalFolders(new_local_path, folder_name)
+        for sub_folder_name in path_sub_folder:
+            createLocalSubFolders(new_local_path, sub_folder_name)  
+        new_local_path = new_local_path.parents[0]
+        path_sub_folder = path_sub_folder_dev 
+    except (EnvironmentError, IOError, OSError) as e:
+        print(e)
