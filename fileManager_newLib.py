55f5a6c760cf5cd03644ad2870715336f51e0f1e
Coutinho William <william.souza@nagra.com>
2020-01-30 18:52:37 -0300

    Alterado a forma que retornava os status das funções de True/False para Numeros. Assim temos 4 estados que podem ser utilizados (0 para baixar, 1 para não baixar, 2 para baixar e criar Version control e 3 para falhas

--- fileManager_newLib.py
+++ fileManager_newLib.py
@@ -5,12 +5,18 @@ import logging
 
 debug = True # Used to avoid Traceback errors when Fails
 
-PATH_FOLDER = path_manager("Version_control")
+PATH_FOLDER = path_manager(r"Z:\01.Opentv5\16.Version_control")
 FILE_NAME = "version_control.txt"
 PATH_COMPLETE = PATH_FOLDER / FILE_NAME 
 #OLDER_BUILD = "6.1.2.55"
-#LATEST_BUILD = "6.1.1.3" 
+#LATEST_BUILD = "6.1.1.5" 
 
+'''
+0 - Go ahead and download
+1 - It's already downloaded
+2 - Go ahead and download. Version control wasn't created yet
+3 - Fail 
+'''
 
 def pathControl(LATEST_BUILD):
     if PATH_FOLDER.exists() and PATH_COMPLETE.exists():
@@ -23,7 +29,7 @@ def pathControl(LATEST_BUILD):
             
     elif not(PATH_FOLDER.exists()):
         print('Folder not found:', PATH_FOLDER)
-        return 0
+        return 3
     
     else:
         print('File not exists. Creating a new one...')
@@ -37,16 +43,17 @@ def checkLatestBuildDownloaded(LATEST_BUILD):
             if OLDER_BUILD == '':
                 print('File is empty. Creating a new one with build name received:',LATEST_BUILD)
                 createFileControl(LATEST_BUILD)
-                return False
+                return 2
             elif OLDER_BUILD != LATEST_BUILD and OLDER_BUILD != '':
                 print(f'Build {LATEST_BUILD} received is different of {OLDER_BUILD} from history.')
                 print('Updating latest build from local file...')
                 createFileControl(LATEST_BUILD)
-                return False
+                return 0
             else:
                 print(f'Build {LATEST_BUILD} received is the same of {OLDER_BUILD} from history.')
                 print('File not updated.')
-                return True
+                return 1
+
         except (EnvironmentError, IOError, OSError)  as e:
             print('Not possible to open history file:', e)
             return e
@@ -70,4 +77,3 @@ def exceptionHandler(exception_type, exception, traceback, debug_hook=sys.except
     else:
         print(f'\t{exception_type.__name__}: {exception}')# Exception.name: exception
 sys.excepthook = exceptionHandler    
-    
