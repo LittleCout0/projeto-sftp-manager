4f0361200a04663cc4c05dbd3e89fa288b79425a
Coutinho William <william.souza@nagra.com>
2020-02-03 12:17:16 -0300

    Atualização do ambiente de log, criação de pasta logs para melhor gerenciamento dos mesmos. Toda mensagem está indo para o log file

--- folderManager_newLib.py
+++ folderManager_newLib.py
@@ -5,59 +5,73 @@ import logging
 debug = True # Used to avoid Traceback errors when Fails
 
 LOCAL_PATH = path_manager(r'Z:\01.Opentv5\00.Versions\NET\Release6.1')
-logging.basicConfig(filename='folder_manager.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+
+##### Log environment ###############################################################################################################################################
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler('logs/folder_manager.log')
+formatter = logging.Formatter('%(asctime)s: %(levelname)s : %(name)s : %(message)s')
+file_handler.setFormatter(formatter)
+log.addHandler(file_handler)
+#####################################################################################################################################################################
 
 def createBuildNameFolder(build_name):
     if(LOCAL_PATH.exists()):
         try:
             print(f'Creating {build_name} folder')
             path_manager.mkdir(path_manager.joinpath(LOCAL_PATH, build_name))
+            log.info(f'Creating {build_name} created')
             return LOCAL_PATH / build_name
         
         except (EnvironmentError, IOError, OSError) as e:
-            logging.exception('An error occured to create build folder')
+            log.exception('An error occured to create build folder')
             print(e)
     
     elif not(LOCAL_PATH.exists()):
         print('Path not found.')
+        log.warning(f'Path not found: {LOCAL_PATH}')
 
 def createSTBModelFolder(new_path, stb_model):
     try:
         print(f'Creating {stb_model} folder')
         path_manager.mkdir(new_path / stb_model)
+        log.info(f'Creating {stb_model} folder')
         return new_path / stb_model
     
     except (EnvironmentError, IOError, OSError) as e:
-        logging.exception('An error occured to create STB model folder')
+        log.exception('An error occured to create STB model folder')
         print(e)
 
 def createBuildTypeFolder(new_path, mw_type):
     try:
         print(f'Creating {mw_type} folder')
         path_manager.mkdir(new_path / mw_type)
+        log.info(f'Creating {mw_type} folder')
         return new_path / mw_type
     
     except (EnvironmentError, IOError, OSError) as e:
-        logging.exception('An error occured to create build type folder')
+        log.exception('An error occured to create build type folder')
         print(e)
         
 def createLocalFolders(new_path,folder_name):
     try:
         path_manager.mkdir(new_path / folder_name)
+        log.info(f'Creating local folder {folder_name}')
         return new_path / folder_name
     
     except (EnvironmentError, IOError, OSError) as e:
-        logging.exception('An error occured to create local folders')
+        log.exception('An error occured to create local folders')
         print(e)
 
 
 def createLocalSubFolders(new_path, sub_folder_name):
     try:
         path_manager.mkdir(new_path / sub_folder_name)
+        log.info(f'Creating sub folder {sub_folder_name}')
         return new_path / sub_folder_name
     
     except (EnvironmentError, IOError, OSError) as e:
-        logging.exception('An error occured to create sub-folders')
+        log.exception('An error occured to create sub-folders')
         print(e)
   
 # Function to not print traceback unless debug is True
